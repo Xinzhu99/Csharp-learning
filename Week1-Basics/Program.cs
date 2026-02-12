28891ede9d489f55d9a1da915913491cfb75a3a0
Xinzhu99 <jxinzhu@gmail.com>
2026-02-12 17:13:00 +0100

    first commit

---
+++ Week1-Basics/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Tapez C pour Celsius ou F pour Fahrenheit");
+
+//lire le choix de l'user et stocker dans variable "choix"
+string choix = Console.ReadLine();
+
+Console.WriteLine("Vous avez tapé : " + choix);
+
+if(choix != "C" || choix!= "F")
+{
+    Console.WriteLine("Erreur, taper C ou F");
+    choix = Console.ReadLine();
+ }
+
+ Console.WriteLine("Choix validé :" + choix);
