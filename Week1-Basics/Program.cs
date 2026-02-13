5a5f4f7efefe40d0f5e663806977ea2f61f6f59f
Xinzhu99 <jxinzhu@gmail.com>
2026-02-13 09:24:45 +0100

    first exo finished

--- Week1-Basics/Program.cs
+++ Week1-Basics/Program.cs
@@ -5,10 +5,28 @@ string choix = Console.ReadLine();
 
 Console.WriteLine("Vous avez tapé : " + choix);
 
-if(choix != "C" || choix!= "F")
+if(choix != "C" && choix!= "F")
 {
     Console.WriteLine("Erreur, taper C ou F");
     choix = Console.ReadLine();
  }
 
  Console.WriteLine("Choix validé :" + choix);
+
+//demander un input de température en string et le convertir en double
+// string input = Console.ReadLine();
+Console.WriteLine("Tapez la température :");
+double temperature = double.Parse(Console.ReadLine());
+Console.WriteLine(temperature);
+
+if(choix == "C")
+{
+    double resultat = (temperature * 9/5) + 32;
+    Console.WriteLine($"Your converted temperature is : {resultat} F");
+}else if (choix == "F")
+{
+    double resultat = (temperature - 32) * 5/9;
+    Console.WriteLine($"Your converted temperature is : {resultat} C");
+
+}
+
